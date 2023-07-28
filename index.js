3fb32531b9ba20e7a4b848283366d7da2b33f420
bharat <bharatpanigrahi225@gmail.com>
2023-07-29 00:31:42 +0530

    added all files

---
+++ index.js
@@ -0,0 +1,18 @@
+let box1 = document.querySelector('#b-1');
+let box2 = document.querySelector('#b-2');
+let box3 = document.querySelector('#b-3');
+
+let box = document.querySelector('#starbuck');
+let test = document.querySelector('.test');
+box1.addEventListener('click', ()=>{
+    box.src = "starbuck-1.png";
+    test.style.backgroundColor = "darkgreen";
+})
+box2.addEventListener('click', ()=>{
+    box.src = "starbuck-2.png";
+    test.style.backgroundColor = "#7e1d62";
+})
+box3.addEventListener('click', ()=>{
+    box.src = "starbuck-3.png";
+    test.style.backgroundColor = "brown";
+})
