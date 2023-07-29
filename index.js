ed1874583d7ad930bea26356fed03151da9e92ff
bharat <bharatpanigrahi225@gmail.com>
2023-07-29 13:01:50 +0530

    added all files

--- index.js
+++ index.js
@@ -16,3 +16,46 @@ box3.addEventListener('click', ()=>{
     box.src = "starbuck-3.png";
     test.style.backgroundColor = "darkgreen";
 })
+
+
+const delivery = document.querySelector('.delivery-img');
+const deliveryContent = document.querySelector('.delivery-content');
+const lastPickUp = document.querySelector('.main-3-pickup');
+const order = document.querySelectorAll('.star');
+
+
+const observer = new IntersectionObserver(
+    (entries) =>{
+        entries.forEach((entry) =>{
+            entry.target.classList.toggle("show", entry.isIntersecting);
+        })
+    }
+)
+const observer1 = new IntersectionObserver(
+    (entries) =>{
+        entries.forEach((entry) =>{
+            entry.target.classList.toggle("show-1", entry.isIntersecting);
+        })
+    }
+)
+const observer2 = new IntersectionObserver(
+    (entries) =>{
+        entries.forEach((entry) =>{
+            entry.target.classList.toggle("show-3", entry.isIntersecting);
+        })
+    }
+)
+const observer3 = new IntersectionObserver(
+    (entries) =>{
+        entries.forEach((entry) =>{
+            entry.target.classList.toggle("show-4", entry.isIntersecting);
+        })
+    }
+)
+
+observer.observe(delivery);
+observer1.observe(deliveryContent);
+observer2.observe(lastPickUp);
+order.forEach((item)=>{
+    observer3.observe(item);
+})
